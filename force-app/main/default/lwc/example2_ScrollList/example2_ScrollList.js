c95c23acad003a413fd95734196ddf9bdf430d75
Paul Roth <paulroth3d@gmail.com>
2020-04-02 11:31:25 -0500

    UPDATE: working example2

--- force-app/main/default/lwc/example2_ScrollList/example2_ScrollList.js
+++ force-app/main/default/lwc/example2_ScrollList/example2_ScrollList.js
@@ -1,3 +1,45 @@
-import { LightningElement } from 'lwc';
+/* eslint-dyisable @lwc/lwc/no-async-operation */
+import { LightningElement, track } from 'lwc';
 
-export default class Example2_ScrollList extends LightningElement {}
+export default class Example2_ScrollList extends LightningElement {
+  rendered = false;
+  @track items = [];
+
+  updateListItems() {
+    this.template.querySelectorAll('li').forEach((li, i) => {
+      li.querySelector('span').innerText = 'List item #' + (i + 1); // update the text content
+      if (i % 2 === 0) {
+        // add striped background for even rows
+        li.style.background = '#ededed';
+      }
+    });
+  }
+
+  onScroll() {
+    for (let i = 0; i < 10; i++) {
+      this.updateListItems();
+    }
+  }
+
+  renderedCallback() {
+    // To avoid input delays when switching nav items, delay with
+    // double rAF and fill the items array once
+    if (this.rendered) {
+      return;
+    }
+    this.rendered = true;
+    /* eslint-disable */
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        /* eslint-enable */
+        console.log('rendered');
+        this.items = Array(500)
+          .fill()
+          .map((_, i) => ({ id: i }));
+        Promise.resolve().then(() => {
+          this.updateListItems();
+        });
+      });
+    });
+  }
+}
